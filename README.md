ae3a0f5b7b6b68afc0a6b6f7bbe2318def007c25
samirrijal <67586338+samirrijal@users.noreply.github.com>
2023-04-11 12:43:04 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# Samir's Portfolio
+
+This is my personal portfolio website built with React. It showcases my skills, projects, and experience in web development. I created this for my React js exam.
+
+## Features
+
+- Responsive design for desktop and mobile devices
+- Portfolio section with my latest projects
+- Skills section showcasing my technical skills and areas of expertise
+- About section with a short bio and contact information
+- Built with modern web development tools and technologies
+
+## Technologies
+
+- React.js
+- HTML
+- CSS
+- JavaScript
+- Bootstrap
+- EmailJS
+
+## Setup
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory and run `npm install` to install the necessary dependencies.
+3. Run `npm start` to start the development server.
+4. Open `http://localhost:3000` in your web browser to view the app.
+
+## Contact
+
+If you have any questions or feedback, feel free to reach out to me using the contact form on my website or via email at [your email address].
+
+Thank you for visiting my portfolio!
---
+++ src/content.js
@@ -0,0 +1,208 @@
+const logotext = "SAMIR";
+const meta = {
+    title: "Samir Rijal",
+    description: "I’m Samir Rijal web designer _ Full stack devloper,currently working in Spain",
+};
+
+const introdata = {
+    title: "I’m Samir Rijal",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I design, develop, and maintain websites using programming languages, frameworks, and tools for optimal user experience.",
+    your_img_url: "https://i.imgur.com/G97fZed.jpg",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "Hi, I'm Samir Rijal, a 22-year-old Nepalese student living in Spain with a passion for full stack web development. I'm currently studying the ins and outs of web development and working hard to become a full stack web developer in the future. I spend my days designing and building websites that not only look great but also offer an exceptional user experience. I'm determined to leave my mark on the web development scene, and with my commitment and talent, I'm confident that I will. So watch out world, because I'm here to make a difference.",
+};
+const worktimeline = [{
+        jobtitle: "Web Design and Development",
+        where: "Buddha Palace Restaurant",
+        date: "2021",
+    },
+    {
+        jobtitle: "Web Design and Marketing",
+        where: "Connect Global Eduaction Consultancy",
+        date: "2022-2023",
+    },
+    {
+        jobtitle: "E-commerce Manager",
+        where: "Yuit The Fair Food Company",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "HTML/CSS",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 85,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Sass/Tailwind/Bootstrap",
+        value: 60,
+    },
+    {
+        name: "Node.js",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "From wireframes to pixel-perfect designs, I specialize in creating stunning UI/UX experiences that leave users impressed. With a focus on usability, accessibility, and modern design trends, my designs will take your website or application to the next level.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "In today's mobile-first world, having a mobile app is a must. I create custom mobile apps for both iOS and Android platforms that are fast, responsive, and user-friendly. With my expertise in mobile app development, I can help bring your ideas to life and make your business stand out.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Looking to build a website on the popular WordPress platform? I've got you covered. With my extensive knowledge of WordPress design, I can create a custom website that is not only visually stunning but also optimized for search engines and user experience. Let me help you take your online presence to the next level with WordPress.",
+    },
+];
+
+const academics = [{
+        title: "High School",
+        where: "Kathmandu BernHardt, Nepal",
+        date: "2018"
+    },
+    {
+        title: "Computer Programming",
+        where: "42 Urduliz, Spain",
+        date: "2022"
+    },
+    {
+        title: "Full Stack Web Development",
+        where: "Fundación EDE, Spain",
+        date: "2023"
+    },
+];
+
+const languages = [{
+        title: "Nepali",
+        description: "My native language.",
+    },
+    {
+        title: "English",
+        description: "Studied my whole life, fluent.",
+    },
+    {
+        title: "Hindi",
+        description: "Learned from Bollywood movies, proficient.",
+    },
+    {
+        title: "Urdu",
+        description: "Similar to Hindi, fluent.",
+    },
+    {
+        title: "Spanish",
+        description: "Lived in Spain for 4 years, fluent.",
+    },
+    {
+        title: "German",
+        description: "Learning for future trip to Germany.",
+    }
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Lorem Ipsum has been the industry's standard dummy text ever.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    EMAIL: "samirrijal0@gmail.com",
+    PHONE: "+34 747-438-944",
+    description: "I'm just a message away! Use the contact form to reach out and let's start collaborating.",
+    SERVICE_ID: "service_xhka4ck",
+    TEMPLATE_ID: "template_vye2k88",
+    USER_ID: "gp-FXH4ulLtd0dZ19",
+};
+
+const socialprofiles = {
+    github: "https://github.com/samirrijal",
+    facebook: "https://www.facebook.com/sameer.rijal.568",
+    linkedin: "https://www.linkedin.com/in/samir-rijal-080601212/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    academics,
+    languages,
+    introdata,
+    contactConfig,
+    socialprofiles,
+    logotext,
+};
